ae900bf197a103f4c411294682c64b05ecc317d4
tekluabayneh <tekluabayneh@gmail.com>
2025-09-25 15:26:59 +0000

    feat: added read file methond

--- main.go
+++ main.go
@@ -1,5 +1,58 @@
 package main
 
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	out := make(chan string, 1)
+	go func() {
+		defer f.Close()
+		defer close(out)
+
+		str := ""
+		for {
+			data := make([]byte, 8)
+			n, err := f.Read(data)
+			if err != nil {
+				break
+			}
+
+			data = data[:n]
+
+			if i := bytes.IndexByte(data, '\n'); i != -1 {
+				str += string(data[:i])
+				data = data[i+1:]
+				out <- str
+				str = ""
+			}
+			str = string(data)
+
+		}
+		if len(str) != 0 {
+			fmt.Printf("read: %s\n", str)
+		}
+
+	}()
+
+	return out
+}
+
 func main() {
+	f, err := os.Open("messages.txt")
+
+	if err != nil {
+		log.Fatal("error", "error", err)
+	}
+
+	lines := getLinesChannel(f)
+
+	for line := range lines {
+		fmt.Printf("read: %s\n", line)
+	}
 
 }
